90be6b29442fee077a68f7860369b8ae68d01759
iZarubin <igorzarubin88gmail.com>
2021-06-06 19:31:46 +0300

    Initial commit

---
+++ app/src/main/java/com/example/calculatorgeekbrains/InputNumbers.java
@@ -0,0 +1,29 @@
+package com.example.calculatorgeekbrains;
+
+public class InputNumbers {
+    private int inputNumber1;
+    private int inputNumber2;
+    private String inputLine;
+
+    public InputNumbers() {
+        inputNumber1 = 0;
+        inputNumber2 = 0;
+    }
+
+    public int getInputNumber1() {
+        return inputNumber1;
+    }
+
+    public void setInputNumber1(int inputNumber1) {
+        this.inputNumber1 = inputNumber1;
+    }
+
+    public int getInputNumber2() {
+        return inputNumber2;
+    }
+
+    public void setInputNumber2(int inputNumber2) {
+        this.inputNumber2 = inputNumber2;
+    }
+
+}
---
+++ app/src/main/java/com/example/calculatorgeekbrains/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.calculatorgeekbrains;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    private String inputLine;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+//inputLine = findViewById(R.id.inputfield);
+//Button button1 = findViewById(R.id.)
+
+    }
+}
